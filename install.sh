d98cd6b3dcf1f83a4b41e0a022e10c32c8ffbb1a
stas <stasbritanishskii@gmail.com>
2024-12-04 19:52:16 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ sudo apt install libglfw3 libglfw3-dev libfreetype6-dev -y
 sudo apt-get install libglm-dev
 wget https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
 sudo mv stb_image.h /usr/local/include/
+ git clone https://github.com/nlohmann/json.git /tmp/json \
+    && cd /tmp/json \
+    && mkdir build && cd build \
+    && cmake .. \
+    && make install
